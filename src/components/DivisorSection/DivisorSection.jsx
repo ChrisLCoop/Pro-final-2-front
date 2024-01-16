07e66ecfe1c30b4f04df52c03d8c69a0bb5905a2
ChrisLCoop <christian.limas.e@gmail.com>
2024-01-16 18:09:18 -0500

    avance0522

---
+++ src/components/DivisorSection/DivisorSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Divisor from '../../assets/divisor-section-down.svg'
+import './divisorsection.css'
+
+const DivisorSection = () => {
+  return (
+    <div className='divisor-section-interm-scsd5'>
+        <img src={Divisor} alt="" />
+    </div>
+  )
+}
+
+export default DivisorSection
--- src/pages/HOME/Home.jsx
+++ src/pages/HOME/Home.jsx
@@ -5,6 +5,13 @@ import Section2 from '../../layouts/SECTION2/Section2'
 import Section3 from '../../layouts/SECTION3/Section3'
 import Section4 from '../../layouts/SECTION4/Section4'
 import Section5 from '../../layouts/SECTION5/Section5'
+import Section7 from '../../layouts/SECTION7/Section7'
+import Section8 from '../../layouts/SECTION8/Section8'
+import Footer from '../../layouts/FOOTER/Footer'
+//import DivisorSection from '../../components/DivisorSection/DivisorSection'
+//import Section6 from '../../layouts/SECTION6/Section6'
+
+
 
 
 const Home = () => {
@@ -15,7 +22,14 @@ const Home = () => {
       <Section2></Section2>
       <Section3></Section3>
       <Section4></Section4>
+      {/* 
+      <DivisorSection></DivisorSection>*/}
       <Section5></Section5>
+      {/* 
+      <Section6></Section6>*/}
+      <Section7></Section7>
+      <Section8></Section8>
+      <Footer></Footer>
     </div>
     </>
   )
