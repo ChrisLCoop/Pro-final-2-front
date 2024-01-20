2e0248debb5b7a06b6d5d6e9124898d40150526d
crisdigital23 <cris.asesordigital7@gmail.com>
2024-01-19 20:31:41 -0500

    frvrvrvr

--- src/layouts/SECTOR/Sector15.jsx
+++ src/layouts/SECTOR/Sector15.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../pages/CURSOS/cursos.css'
-
+import imagen11 from '../../assets/imgcurso/image-removebg-preview.png'
 function Sector15() {
   return (
     <>
@@ -14,7 +14,7 @@ function Sector15() {
             <p>Aprenda Git, Docker y Kubernetes y comprenda la entrega continua. Estudia administración de redes, Linux y administra servidores en la nube. Explora el mundo del Internet de las cosas y la robótica. sepa cómo empezar con DevOps. Encuentra mas sobreEscuela DevOps navega por esta página para ver todos nuestros cursos y capacitaciones.</p>
         </div>
         <div class="cursos2-1-2">
-            <img src="src/assets/imgcurso/image-removebg-preview.png" alt=""/>
+            <img src={imagen11} alt=""/>
         </div>
     </div>
     </section>
